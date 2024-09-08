6c00307a0b8830cee87e5f11286a8506145c66f4
Justin Haddock <jhaddoc5@ut.utm.edu>
2024-09-07 22:48:43 -0500

    Changed: main.cpp is now a sum calculator.

--- main.cpp
+++ main.cpp
@@ -3,8 +3,21 @@
 
 using namespace std;
 
+int sum(int x);
+
 int main() {
-  cout << "Hello, World!";
+  int input;
   
+  cout << "Enter a number: ";
+  cin >> input;
+  cout << sum(input);
   return 0;
+}
+
+int sum(int x) {
+  int y = 1;
+  for(int i = 0;i < x; i++) {
+    y += i;
+  }
+  return y;
 }
